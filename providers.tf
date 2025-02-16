d5ebf452cb83618b494a7b05b5a2badd2954ebfa
NvAq <154682799+NvAq@users.noreply.github.com>
2025-02-16 18:59:19 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,22 @@
+# providers.tf
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+}
+
+provider "aws" {
+  region = var.aws_region
+}
